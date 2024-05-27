75727e1229f892ca13fbf8bd8cfd7f2c510bbaaf
VinkoRobi <Vinkolino178@gmail.com>
2024-05-27 12:21:54 -0500

    Ansley

--- Remeinders/src/App.jsx
+++ Remeinders/src/App.jsx
@@ -1,14 +1,34 @@
 import Form from './Componentes/Form'
 import Task from './Componentes/Task'
 import './App.css'
+import { useState } from 'react'
 
 const App = ()=> {
-
+const [input,setInput] = useState("")
+const [input2,setInput2] = useState("")
+const [texto,settexto] = useState("")
+const [tareas,setTarea] = useState([])
+const HandleChange = ({target})=>{
+  setInput(target.value)
+}
+const HandleChange2 = ({target})=>{
+  setInput2(target.value)
+}
+const Add = (e)=>{
+ e.preventDefault()
+ if(input.trim() !== "" && input2.trim() !== ""){
+  setTarea([...tareas,input])
+  settexto([...texto,input2])
+  setInput2("")
+  setInput("")
+ }
+ 
+}
 
   return (
     <div className='min-h-screen bg-gradient-to-r from-blue-900 via-cyan-500 to-blue-500'>
       <h1 className='grid place-content-center text-white text-2xl '>Todo-App</h1>
-      <Form />
+      <Form  HandleChange = {HandleChange} Add = {Add} input = {input} input2 = {input2} HandleChange2={HandleChange2} tareas={tareas} texto = {texto}/>
     </div>
   )
 }
--- Remeinders/src/Componentes/Form.jsx
+++ Remeinders/src/Componentes/Form.jsx
@@ -1,30 +1,10 @@
-import { Input } from "postcss";
+
 import { useState } from "react";
-const Form = ()=>{
-    const [inputv,setinputv] = useState('')
-    const [input2,setinputv2] = useState('')
-    const [text,settext] = useState("")
-    const [items, setItems] = useState([]);
-    const HandleSubmit = ({target})=>{
-     setinputv(target.value)
-    }
-    const HandleSubmit2 = ({target})=>{
-      setinputv2(target.value)
-    }
-    const HandleAdd = (e) => {
-      e.preventDefault();
-      if (inputv.trim() !== '' && input2.trim() !== '') {
-        setItems([...items, inputv]);
-        settext([...text, input2]);
-        setinputv('');
-        setinputv2('');
-      } else {
-        alert('Por favor ingrese valores en ambos campos');
-      }
-    };
+const Form = ({Add,HandleChange,input,input2,HandleChange2,tareas,texto})=>{
+  
     return (
         <div className="max-w-md mx-auto mt-8">
-          <form onSubmit={HandleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <form onSubmit={HandleChange} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
   <div className="mb-4">
     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="item">
       Ingrese un valor:
@@ -33,8 +13,8 @@ const Form = ()=>{
       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
       id="item"
       type="text"
-      value={inputv}
-      onChange={HandleSubmit}
+      value={input}
+      onChange={HandleChange}
       placeholder="Ingrese un valor"
     />
   </div>
@@ -47,12 +27,12 @@ const Form = ()=>{
       id="otro"
       type="text"
       value={input2}
-      onChange={HandleSubmit2}
+      onChange={HandleChange2}
       placeholder="Otro campo"
     />
   </div>
   <div className="flex items-center justify-end">
-    <button onClick={HandleAdd}
+    <button onClick={Add}
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
       type="submit"
     >
@@ -62,15 +42,17 @@ const Form = ()=>{
 </form>
 
           <ul className="list-disc list-inside">
-                {items.map((item, index) => (
+          {tareas.map((item, index) => (
                     <div className="max-w-sm rounded overflow-hidden shadow-lg mb-12">
                     <div className="bg-gradient-to-r from-blue-500 to-green-500 p-4">
                       <li className="text-white text-base " key={index}>{item} </li>
-                      <p className="text-white text-base" key={index}>{text[index]}</p>
+                      <p className="text-white text-base" key={index}>{texto[index]}</p>
                       
                     </div>
                   </div>
-                ))}
+                ))} 
+
+                
             </ul>
         </div>
       );
